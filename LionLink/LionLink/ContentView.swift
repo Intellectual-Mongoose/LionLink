77b3c5036ee25f64f135d9413405613bf7b95310
Joshua Eerkes <joshua.eerkes@ksd.org>
2020-01-28 19:32:14 -0800

    Minor Changes

---
+++ LionLink/LionLink/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LionLink
+//
+//  Created by Joshua Eerkes on 1/28/20.
+//  Copyright © 2020 Kennewick Lions. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
