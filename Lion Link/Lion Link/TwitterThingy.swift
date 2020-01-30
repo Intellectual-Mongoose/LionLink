4df6947be35258a637da87b18e7ce359bed0c727
Intellectual-Mongoose <60476150+Intellectual-Mongoose@users.noreply.github.com>
2020-01-30 14:34:51 -0800

    Add files via upload

---
+++ Lion Link/Lion Link/TwitterThingy.swift
@@ -0,0 +1,30 @@
+//
+//  TwitterThingy.swift
+//  Lion Link
+//
+//  Created by Sean Francis on 1/28/20.
+//  Copyright © 2020 KeHS Comp Sci. All rights reserved.
+//
+
+import SwiftUI
+
+struct TwitterThingy: View {
+    var body: some View {
+        
+        
+        
+        
+        Text("")
+        
+        
+        
+        
+        
+    }
+}
+
+struct TwitterThingy_Previews: PreviewProvider {
+    static var previews: some View {
+        TwitterThingy()
+    }
+}
